373996ade1e7ac898b03801604f52b739a0a19e9
petergich <petergich80@gmail.com>
2024-03-09 20:02:44 +0300

    "commit "

--- carapp/urls.py
+++ carapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlPatterns=[
-  path("",views.home,name="home")
+urlpatterns = [
+    path("",views.home,name="home")
 ]
+
--- cardealer/settings.py
+++ cardealer/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-9kfy35nym$_-$h30o99z58-15bl@l!2tlhuw)!1ga+dt6z4f(1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'carapp.apps.CarappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'cardealer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
