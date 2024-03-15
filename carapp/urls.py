6b8d997925e001cde7c7c9585b0d89b6984efb83
petergich <petergich80@gmail.com>
2024-03-15 16:03:31 +0300

    commit

--- carapp/urls.py
+++ carapp/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home,name="home")
+    path("",views.home,name="home"),
+    path("cars",views.cars,name="cars"),
+    path("about",views.about,name="about"),
+    path("blog",views.aboutblog,name="blog"),
+    path("blogdetails",views.aboutblogdetails,name="blogdetails"),
+    path("team",views.aboutteam,name="team"),
+    path("testimonials",views.abouttestimonials,name="testimonials"),
+    path("faq",views.aboutfaq,name="faq"),
+    path("terms",views.aboutterms,name="terms"),
+    path("contact",views.contact,name="contact"),
+    path("cardetais",views.cardetails,name="cardetails"),
 ]
 
--- carapp/views.py
+++ carapp/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
 def home(request):
+  return render(request,"index.html")
+def cars(request):
   return render(request,"cars.html")
+def about(request):
+  return render(request,"about.html")
+def aboutblog(request):
+  return render(request,"blog.html")
+def aboutblogdetails(request):
+  return render(request,"blog-details.html")
+def aboutteam(request):
+  return render(request,"team.html")
+def abouttestimonials(request):
+  return render(request,"testimonials.html")
+def aboutfaq(request):
+  return render(request,"faq.html")
+def aboutterms(request):
+  return render(request,"terms.html")
+def contact(request):
+  return render(request, "contact.html")
+def cardetails(request):
+  return render(request,"car-details.html")
 # Create your views here.
